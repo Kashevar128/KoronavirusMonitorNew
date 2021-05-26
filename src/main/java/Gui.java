4f9d10567ea2ddc278015e5a5e6705ab1eebf17c
Kashevar128 <steve128@yandex.ru>
2021-05-26 23:28:41 +0300

    Update 008

--- src/main/java/Gui.java
+++ src/main/java/Gui.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
@@ -9,21 +10,27 @@ public class Gui extends JFrame {
     public Gui() {
         super("Монитор эпидемиологической обстановки в регионах России");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(new BorderLayout());
         JPanel contents = new JPanel();
+        JPanel dop = new JPanel();
+        dop.setLayout(new FlowLayout());
         parser = new Parser();
         String[] listStr = parser.splitter(parser.toString());
         final JList<String> list = new JList<>(listStr);
         list.setSelectionMode (ListSelectionModel.SINGLE_SELECTION);
-        list.setPrototypeCellValue("Республика Северная Осетия-Алания");
-        list.setVisibleRowCount(18);
+        JTextField search = new JTextField(null, 20);
+        JLabel label = new JLabel("Поиск:");
+        JButton button = new JButton("Найти");
         content = new JTextArea(20, 25);
         content.setLineWrap(true);
+        content.setEditable(false);
         content.setWrapStyleWord(true);
         list.addListSelectionListener(new Listener());
         list.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                if(e.getClickCount() == 2) {
+                if(e.getClickCount() == 1) {
                     int selected = list.locationToIndex(e.getPoint());
                     String txt;
                     txt = parser.info(listStr[selected]);
@@ -31,13 +38,15 @@ public class Gui extends JFrame {
                 }
             }
         });
-
-        contents.add(new JLabel("Список регионов"));
-        contents.add(new JScrollPane(list));
-        contents.add(new JLabel("Статистика"));
-        contents.add(new JScrollPane(content));
+        dop.add(label);
+        dop.add(search);
+        contents.setLayout(new BorderLayout());
+        contents.add(new JScrollPane(list), BorderLayout.WEST);
+        contents.add(new JScrollPane(content), BorderLayout.CENTER);
+        contents.add(dop, BorderLayout.NORTH);
+        setLayout(new BoxLayout(contents, BoxLayout.X_AXIS));
         setContentPane(contents);
-        setSize(760, 400);
+        setSize(550, 400);
         setResizable(false);
         setVisible(true);
     }
--- src/main/java/TestParser.java
+++ src/main/java/TestParser.java
@@ -1,7 +1,6 @@
 public class TestParser {
     public static void main(String[] args) {
         Parser pars = new Parser();
-        pars.info("Севастополь");
         pars.splitter(pars.toString());
         new Gui();
     }
