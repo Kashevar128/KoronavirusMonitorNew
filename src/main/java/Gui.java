671a2863888bb225481723423d485507f89667c1
Kashevar128 <steve128@yandex.ru>
2021-05-27 22:41:28 +0300

    Update 008

--- src/main/java/Gui.java
+++ src/main/java/Gui.java
@@ -1,7 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 
 public class Gui extends JFrame {
     private final Parser parser;
@@ -17,8 +20,8 @@ public class Gui extends JFrame {
         dop.setLayout(new FlowLayout());
         parser = new Parser();
         String[] listStr = parser.splitter(parser.toString());
-        final JList<String> list = new JList<>(listStr);
-        list.setSelectionMode (ListSelectionModel.SINGLE_SELECTION);
+        JList<String> list = new JList<>(listStr);
+        list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         JTextField search = new JTextField(null, 20);
         JLabel label = new JLabel("Поиск:");
         JButton button = new JButton("Найти");
@@ -29,13 +32,55 @@ public class Gui extends JFrame {
         list.addListSelectionListener(new Listener());
         list.addMouseListener(new MouseAdapter() {
             @Override
-            public void mouseClicked(MouseEvent e) {
-                if(e.getClickCount() == 1) {
-                    int selected = list.locationToIndex(e.getPoint());
+            public void mouseClicked(MouseEvent mouseEvent) {
+                if (mouseEvent.getClickCount() == 1) {
+                    int selected = list.locationToIndex(mouseEvent.getPoint());
                     String txt;
                     txt = parser.info(listStr[selected]);
                     content.setText(txt);
                 }
+            }
+        });
+        list.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent keyEvent) {
+                ArrayList<Integer> countMemory = new ArrayList<>();
+                String message = search.getText();
+                if(message != null) {
+                    for(String s: listStr) {
+                        Integer count = 0;
+                        if(message.length() <= s.length()) {
+                            for (int i = 0; i < message.length(); i++) {
+                                if(message.charAt(i) == s.charAt(i)) {
+                                    count++;
+                                } else break;
+                            }
+                        } else {
+                            for (int i = 0; i < s.length(); i++) {
+                                if(message.charAt(i) == s.charAt(i)) {
+                                    count++;
+                                } else break;
+                            }
+                        }
+                        countMemory.add(count);
+                    }
+
+                    int max = 0;
+                    for (int a : countMemory) {
+                        max = Math.max(max, a);
+                    }
+
+                    if(max != 0) {
+                        DefaultListModel model = (DefaultListModel) list.getModel();
+                        for (int i = 0; i < model.size(); i++) {
+                            if(countMemory.get(i) != max) {
+                                model.remove(i);
+                            }
+                        }
+                    }
+                }
+
+
             }
         });
         dop.add(label);
@@ -49,5 +94,7 @@ public class Gui extends JFrame {
         setSize(550, 400);
         setResizable(false);
         setVisible(true);
+
     }
+
 }
